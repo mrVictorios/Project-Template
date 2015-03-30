5ed9014df5a44df693234ed146066bb3dafd541e
victorios <victorios@me.com>
2015-03-30 14:19:03 +0200

    optimize xdebug statement

--- puppet/modules/php5/manifests/xdebug.pp
+++ puppet/modules/php5/manifests/xdebug.pp
@@ -3,7 +3,8 @@ define php5::xdebug(
   $remote_handler      = "dbgp",
   $remote_port         = 9000,
   $remote_autostart    = true,
-  $remote_connect_back = true) {
+  $remote_connect_back = true,
+  $remote_idekey       = 'PHPSTORM') {
 
   package { "php5-xdebug":
     require => Package['php5-dev']
@@ -16,8 +17,8 @@ define php5::xdebug(
   }
 
   exec { "set xdebug path":
-    command => "/bin/bash -c \"export phpextension=$(php-config --extension-dir); echo \"zend_extension = \\\${phpextension}/xdebug.so\" >> /etc/php5/mods-available/xdebug.ini\"",
-    require => File['/etc/php5/mods-available/xdebug.ini']
+    command => "echo \"zend_extension = $(php-config --extension-dir)/xdebug.so\" >> /etc/php5/mods-available/xdebug.ini",
+    require => [File['/etc/php5/mods-available/xdebug.ini'], Package['php5-xdebug']]
   }
 
   php5::enmod { "enable xdebug":
